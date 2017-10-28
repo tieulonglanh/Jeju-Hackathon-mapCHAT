e87fe1707384e5a7b05f38a8d1ee2591b199dbe4
LIM YOUN SOO <maekung3@gmail.com>
2017-10-28 18:59:51 +0900

    set the form of second page

--- learn.php
+++ learn.php
@@ -1,21 +1,25 @@
 <html>
 <head>
-   <link rel="stylesheet" type="text/css" href="testing.css">
+   <link rel="stylesheet" type="text/css" href="learn.css">
 </head>
 
 <body class = "rectangle-11">
 
 <div class = "rectangle-3" >
-  <img src = "Rectangle 3.png">
+  <img src = "Rectangle 3.png" />
+</div>
 
   <img class = "wordings" src = "Wordings.png" />
-</img>
+  <img class = "iot" src = "IOT.png" />
+  <img class = "bigdata" src = "Big Data.png" />
+  <img class = "analytics" src= "Analytics.png" />
 
-</div>
 
 <img class = "iphone-mockup" src= "iphone Mockup.png" />
 
 <img class = "logo" src = "Logo.png" />
+
+
 
 
 </body>
