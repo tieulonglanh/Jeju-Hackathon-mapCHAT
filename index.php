e3b6e2cb81e79944a43ee8a66cff8f548c850d3e
lmasi <lmasi@naver.com>
2017-10-29 08:30:34 +0900

    sync

--- index.php
+++ index.php
@@ -1,44 +1,36 @@
 <?php
 
 echo'
+
 <html>
 <head>
-   <link rel="stylesheet" type="text/css" href="./css/mystyle.css">
+   <link rel="stylesheet" type="text/css" href="./css/relative.css">
 </head>
 
 <body>
-  <div class="iphone-moc-kup">
+<div class="total">
+  <div class="left-content">
+    <img class="main_img" src="./img/img.png"/>
+  </div>
+    <div class="right-content">
+      <div class="left-top">
 
-      <div class="rectangle-3">
+          <img class="oval-3" src="./img/learn.png">
+    </div>
+    <div class="left-mid">
+      <img class="logo" src="./img/words.png"/>
 
-      </div>
-
-      <img class="oval" src="./img/data.png"/>
-      <img class="lighting" src="./img/iphone MocKup.png"/>
-      <img src="./img/Save Money Icon.png" class="save-money-icon"/>
-      <img src="./img/Tracking Icon.png" class="tracking"/>
-      <img class="ecobuddychatbot" src="./img/ecobuddy(chatbot).png"/>
-      <img class="logo-2" src="./img/Logo 2.png"/>
-      <div class="cost-effective-livin">
-        Cost Effective Living
-      </div>
-      <div class="ecobuddy-chat-bot-en"> ecobuddy Chat bot encourages you be more environmentally cautions while saving you money</div>
-      <div class="ecobuddy-help-me-li">"ecobuddy help me live a more environmental cautious lifestyle by reminding me to use less water, to turning off unused appliances, to even taking the time to enjoy quality air when and where I can"</div>
-      <div class="ecobuddy-help-me-li2">-Nia Long (Working Mom)</div>
-      <div class="get-eco-buddy-on-mess">Get ecoBuddy on Messenger</div>
-        <a href="https://www.messenger.com/t/9day2u"><img class="messenger" src="./img/Messenger.png"/></a>
-        <div  class="learn-more-here">Learn more here</div>
-
-
-      /*chagne here*/
-      <div class="chat_box" id="chat_box">
-          <div>BLANK</div>
-          <div>div id=chat_box</div></div>
-      </div>
-
-
+    </div>
+    <div class="left-bottom">
+      <a class="left-bottom" href="https://www.messenger.com/t/9day2u">
+      <img class="messenger" src="./img/messenger.png"/>
+    </a>
+    </div>
+    </div>
+</div>
 </body>
 
 </html>
+
 ';
 ?>
