283c18331940bd0a08235b19ab6b5b8510bc26f3
LIM YOUN SOO <maekung3@gmail.com>
2017-10-29 06:29:20 +0900

    final second page

--- learn.php
+++ learn.php
@@ -1,27 +1,53 @@
 <html>
 <head>
-   <link rel="stylesheet" type="text/css" href="learn.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" />
+  <link rel="stylesheet" type="text/css" href="/css/learn.css">
+
 </head>
+<body>
+  <body>
+    <div class="container">
+        <div class="left col-sm-12 col-md-3">
+          <div class="logobox col-sm-12 col-md-12">
+            <a href="/"><img class="logo" src="/img/Logo.png"></a>
+          </div>
 
-<body class = "rectangle-11">
+          <div class="iphonebox col-sm-12 col-md-12">
+            <img class="iphone" src="/Iphone Mockup.png">
+          </div>
+        </div>
 
-<div class = "rectangle-3" >
-  <img src = "Rectangle 3.png" />
-</div>
+        <div class="right col-sm-12 col-md-9">
+          <div class="middlebox">
+            <div class="txtbox">
+              <p>ecoBuddy is an environmental friendly services that reminds you to save the planet by being more environmental cautions in your daily life. We at Team ecoBuddy understand the diffiulties and challenges that come with practicing mindful environmental lifestyle.
+              </p><p>ecoBuddy helps you practice a more sustainable environmental lifestyle from the very start of your day to when you lay your head to rest. You can interact with ecoBuddy to help you reduce energy, make cost
+              effective decision and even appreciate quality air around you.</p>
+              <p>Together, we all make a grander impact on the world.</p>
+            </div>
 
-  <img class = "wordings" src = "Wordings.png" />
-  <img class = "iot" src = "IOT.png" />
-  <img class = "bigdata" src = "Big Data.png" />
-  <img class = "analytics" src= "Analytics.png" />
+            <div class="imgbox">
+              <div class="col-md-3">
+                <img class="img-responsive"  src="img/IOT.png"></img>
+              </div>
 
+              <div class="col-md-3">
+                <img class="img-responsive" src="img/Big Data.png"></img>
+              </div>
 
-<img class = "iphone-mockup" src= "iphone Mockup.png" />
-
-<img class = "logo" src = "Logo.png" />
-
-
-
+              <div class = "col-md-3">
+                <img class="img-responsive" src ="img/Analytics.png"></img>
+              </div>
+              <div class="clearfix"></div>
+            </div>
+            <div class="clearfix"></div>
+          </div>
+          <div class="clearfix"></div>
+        </div>
+    </div>
+  </body>
 
 </body>
-
 </html>
